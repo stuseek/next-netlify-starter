01d9ff1b7cad208ff2911e2a1153e47d5b2f733d
Stan <stanislav.stepanenko@borngroup.com>
2023-02-28 21:45:16 +0200

    mocks

---
+++ mockserver.js
@@ -0,0 +1,12 @@
+const defaultConfig = require("dotenv-defaults");
+const dotenv = require("dotenv");
+const dotenvExpand = require("dotenv-expand");
+const http = require("http");
+const mockserver = require("mockserver");
+
+defaultConfig.config();
+
+const myEnv = dotenv.config();
+dotenvExpand.expand(myEnv);
+
+http.createServer(mockserver("mocks", { verbose: true })).listen(9001);
